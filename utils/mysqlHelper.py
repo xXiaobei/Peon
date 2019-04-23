2d74ed4d00b742267e60c4df62abc4def59338d7
xXiaobei <10421310@qq.com>
2019-04-23 16:37:49 +0800

    数据库操作辅助类

--- utils/mysqlHelper.py
+++ utils/mysqlHelper.py
@@ -1,49 +1,61 @@
 # encoding:utf-8
 import pymysql
+import json
+
+# mysql 数据库配置
+mysql_config = {
+    "host":"",
+    "port":3306,
+    "user":"",
+    "password":"",
+    "database":"",
+    "charset":"utf-8"
+}
 
 
-# 数据库辅助类
-class DB_Helper:
-
-    def __init__(self, host, user, pwd, port, db_name, charset='utf8'):
-        self.host = host
-        self.user = user
-        self.pwd = pwd
-        self.port = port
-        self.db_name = db_name
-        self.charset = charset
-
+# 使用with简化资源管理 with 知识点？
+# 根据配置文件生成对应的mysql持久会话
+class mysql_adapter():
+    def __init__(self):
         try:
-            self.con = pymysql.connect(host=host, user=user, password=pwd, db=db_name, charset=charset,
-                                       cursorclass=pymysql.cursors.DictCursor)
-        except:
-            return False
-        self.cur = self.con.cursor()
-        return True
+            self.db_con = pymysql.Connect(**mysql_config)
+            self.db_cur = self.db_con.cursor(pymysql.cursors.DictCursor)
+        except expression as identifier:
+            print("mysql db init unsuccess!")
+    
+    def __enter__(self):
+        return self.db_cur
+        
+    def __exit__(self):
+        self.db_cur.close()
+        self.db_con.close()
 
-    # 关闭链接
-    def close_connection(self):
-        if self.cur and self.con:
-            self.cur.close()
-            self.con.close()
+# 设置当前数据库的配置信息
+def set_mysql_config(db_conf):
+    if db_conf:
+        if db_conf["host"]:
+            mysql_config["host"] = db_conf["host"]
+        if db_conf["port"]:
+            mysql_config["port"] = db_conf["port"]
+        if db_conf["user"]:
+            mysql_config["user"] = db_conf["user"]
+        if db_conf["password"]:
+            mysql_config["password"] = db_conf["password"]
+        if db_conf["database"]:
+            mysql_config["database"] = db_conf["database"]
+        if db_conf["charset"]:
+            mysql_config["charset"] = db_conf["charset"]
 
-    # 获取当前数据库所有的用户表
-    def get_customer_tables(self):
-        if self.con and self.cur:
-            sql_tables = 'SHOW DATABASES '
-            return self.cur.execute(sql_tables)
-        return None
+# 获取当前数据库汇总所有的数据库名
+def get_db_names(db_conf):
+    with(mysql_adapter() as db):
+        sql_db_name = 'SELECT DATABASES'
+        db.execute(sql_db_name)
+        return json.dumps(db.fetchall())
+    return None
 
-    # 获取当前网站的栏目
-    def get_archive_types(self, dbname):
-        if dbname and self.con and self.cur:
-            sql_types = 'SELECT * FROM %s.dede_arctype' % dbname
-            return self.cur.execute(sql_types)
-        return None
+# 获取当前站点相关信息
+def get_site_info(db_cnf):
+    with(mysql_adapter() as db):
+        sql_site_info = 'SELECT '
 
-    # 获取网站主要配置信息
-    def get_website_conf(self, dbname):
-        if dbname and self.con and self.cur:
-            sql_conf = "SELECT 'cfg_basehost','cfg_webname','cfg_keywords','cfg_description','cfg_beian' FROM %s.dede_sysconfig" % dbname
-            return self.cur.execute(sql_conf)
-        return None
